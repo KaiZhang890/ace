f610057b5ce54b6078e491e9ae147b2628c6302c
zhangkai <82190688@qq.com>
2017-12-20 10:13:11 +0800

    remove unused code

--- ace2/ace2.go
+++ ace2/ace2.go
@@ -11,47 +11,13 @@ import (
 
 const (
 	// ConnectHost is a host
-	ConnectHost = "127.0.0.1"
+	ConnectHost = ""
 	// ConnectPort is a port
 	ConnectPort = "9000"
 	// ConnectType is a type
 	ConnectType = "tcp"
 )
 
-/*
-func (u *user) handleInput(userInput string) {
-	switch u.state {
-	case stateNaming:
-		if utf8.RuneCountInString(userInput) > 0 {
-			u.name = userInput
-			u.state = stateIdle
-
-			str := fmt.Sprintf("%s, are you ready?[Y/n]", u.name)
-			u.conn.Write([]byte(str))
-		} else {
-			u.conn.Write([]byte("\ntype your name: "))
-		}
-	case stateIdle:
-		if strings.ToUpper(userInput) == "Y" {
-			u.state = stateMatching
-			u.conn.Write([]byte("Matching..."))
-		} else {
-			str := fmt.Sprintf("%s, are you ready?[Y/n]", u.name)
-			u.conn.Write([]byte(str))
-		}
-	case stateMatching:
-		u.conn.Write([]byte("Matching..."))
-	case stateStart:
-		if len(userInput) > 0 {
-			for _, user := range u.group {
-				str := fmt.Sprintf("%s say: %s\n", u.name, userInput)
-				user.conn.Write([]byte(str))
-			}
-		}
-	}
-}
-
-*/
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
@@ -143,46 +109,6 @@ func handleMatching() {
 		time.Sleep(1000 * time.Millisecond)
 	}
 }
-
-/*
-func handleMatching() {
-	for {
-		var group []*user
-		for _, user := range allUser {
-			if user.state == stateMatching {
-				group = append(group, user)
-				if len(group) == 3 {
-					break
-				}
-			}
-		}
-
-		if len(group) == 3 {
-			str := fmt.Sprintf("\n%s, %s, %s in a group\n", group[0].name, group[1].name, group[2].name)
-			for _, user := range group {
-				user.group = group
-				user.state = stateStart
-				user.conn.Write([]byte(str))
-			}
-			startGame(group)
-		}
-
-		time.Sleep(1000 * time.Millisecond)
-	}
-}
-
-func startGame(group []*user) {
-	d1, d2, d3, _ := poker.Deal()
-
-	str := fmt.Sprintf("You got:%s\n", d1)
-	group[0].conn.Write([]byte(str))
-	str = fmt.Sprintf("You got:%s\n", d2)
-	group[1].conn.Write([]byte(str))
-	str = fmt.Sprintf("You got:%s\n", d3)
-	group[2].conn.Write([]byte(str))
-
-}
-*/
 
 func e(err error) {
 	if err != nil {
